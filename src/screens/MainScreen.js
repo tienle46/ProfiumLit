73441b73575e85be372aefb4c2de740803be2179
Tiến Lê <tienle4695@gmail.com>
2018-11-15 02:47:46 +0200

    add mainscreen

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react'
 import {View, Text} from 'react-native'
+import API from '../cores/API'
 
 export default class MainScreen extends Component{
+
     render() {
         return(
             <Text>asdasd</Text>
